4718069e280c39286ebf5081d1433ed2371269c1
bipa <bard.pettersen@intelegosystems.no>
2017-03-06 12:49:28 +0100

    remove  the server folder from git

--- server/simulation/station.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Station {
-    constructor(name) {
-        this.name = name;
-    }
-}
-exports.Station = Station;
-//# sourceMappingURL=station.js.map
